7a11c176970035d3f40c726407c1a0fd2b88ec73
Karitham <kar@karitham.dev>
2025-02-17 18:57:23 +0100

    update lanzaboote

--- hosts/belaf/default.nix
+++ hosts/belaf/default.nix
@@ -8,29 +8,73 @@
   catppuccin.flavor = "macchiato";
   desktop.enable = true;
   hm.enable = true;
+  time.timeZone = "Europe/Paris";
 
   system = {
     stateVersion = "24.05";
   };
 
-  environment.systemPackages = [
-    # For debugging and troubleshooting Secure Boot.
-    pkgs.sbctl
-  ];
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.lanzaboote = {
     enable = true;
     pkiBundle = "/etc/secureboot";
   };
+  boot.supportedFilesystems = ["bcachefs"];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.configurationLimit = 10;
+
+  networking.networkmanager.enable = true;
+
+  services.pcscd.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+  };
+  services = {
+    tailscale.enable = true;
+    tailscale.useRoutingFeatures = "client";
+
+    # touchpad
+    touchegg.enable = true;
+
+    # bluetooth
+    blueman.enable = true;
+
+    # udev
+    udev.packages = [pkgs.via];
+  };
+
+  hardware.keyboard.qmk.enable = true;
+
+  security = {
+    sudo.wheelNeedsPassword = false;
+    rtkit.enable = true;
+  };
+
+  programs.ssh.startAgent = true;
+  environment = with pkgs; {
+    systemPackages = [
+      sbctl
+      vscode
+      fzf
+      sd
+      mpv
+      busybox
+      moreutils
+      age
+      wireguard-tools
+      helix
+    ];
+  };
+
+  virtualisation.docker.enable = true;
+  virtualisation.docker.daemon.settings = {
+    data-root = "/docker";
+  };
 
   home-manager.users.kar.imports = [./desktop.nix];
-
   imports = [
     inputs.lanzaboote.nixosModules.lanzaboote
     inputs.catppuccin.nixosModules.catppuccin
-    inputs.home-manager.nixosModules.home-manager
-    ../../modules/home
     ./hardware.nix
-    ./configuration.nix
   ];
 }
