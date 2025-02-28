6dc702956ea98fdb20c13aac547e7a3cda90aa61
karitham <kar@karitham.dev>
2025-02-28 22:21:37 +0000

    update flake, remove biome build overrides, add cachix

---
+++ modules/nixos/cachix.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  nix.settings = {
+    substituters = lib.mkAfter [
+      "https://karitham.cachix.org"
+    ];
+
+    trusted-public-keys = lib.mkAfter [
+      "karitham.cachix.org-1:Q0wdHZsCssuepIrtx83gHibE0LTDYLVNnvaV3Nms9U0="
+    ];
+  };
+
+  environment.systemPackages = [pkgs.cachix];
+}
