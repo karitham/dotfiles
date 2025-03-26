611723cb951906b1bd16e44005619c94650c7e1b
karitham <kar@karitham.dev>
2025-03-26 11:22:47 +0100

    discord remove dumb bar

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -8,6 +8,46 @@
     home.packages = [pkgs.legcord];
     xdg.configFile."legcord/quickCss.css".text = ''
       @import url("https://catppuccin.github.io/discord/dist/catppuccin-macchiato.theme.css");
+
+      /* Hide top bar */
+      .visual-refresh {
+        --custom-app-top-bar-height: 0px !important;
+      }
+
+      .visual-refresh .chat_f75fb0[data-has-border="true"],
+      .visual-refresh .container__133bf,
+      .visual-refresh .container_a592e1 {
+        border-top: 0 !important;
+      }
+
+      .visual-refresh .sidebarListRounded_c48ade {
+        border-top: 0 !important;
+        border-top-left-radius: 0 !important;
+      }
+
+      .visual-refresh .scroller_ef3116 {
+        padding-block: var(--space-md) !important;
+      }
+
+      /* Compact horizontal padding on server list */
+      .visual-refresh {
+        --custom-guild-list-padding: min(var(--space-sm)) !important;
+      }
+
+      /* Hide download app on server list */
+      .listItem__650eb:has([data-list-item-id="guildsnav___app-download-button"]) {
+        display: none !important;
+      }
+
+      /* Hide Nitro and Discord Shop tab */
+      .channel__972a0:has(a[href="/shop"], a[href="/store"]) {
+        display: none !important;
+      }
+
+      /* Hide gift button in chat message input */
+      .buttons__74017 .button__201d5[aria-label="Send a gift"] {
+        display: none !important;
+      }
     '';
   };
 }
