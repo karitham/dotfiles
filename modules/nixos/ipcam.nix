2104fc63eaf8bd257c4d954a45b16a6cf342c829
karitham <kar@karitham.dev>
2025-04-16 12:15:48 +0200

    ipcam script for local ip camera over usb

--- modules/nixos/ipcam.nix
+++ modules/nixos/ipcam.nix
@@ -15,6 +15,18 @@
       '';
     };
 
-    environment.systemPackages = [pkgs.ffmpeg];
+    environment.systemPackages = let
+      port = 9696;
+      ipcam = pkgs.writeShellScriptBin "ipcam" ''
+        ${lib.getExe' pkgs.android-tools "adb"} wait-for-usb-device
+        ${lib.getExe' pkgs.android-tools "adb"} forward tcp:${toString port} tcp:8080
+        ${lib.getExe pkgs.ffmpeg} -i http://localhost:${toString port}/video -vf format=yuv420p -f v4l2 /dev/video9
+        ${lib.getExe' pkgs.android-tools "adb"} forward --remove tcp:${toString port}
+      '';
+    in [
+      pkgs.ffmpeg
+      pkgs.android-tools
+      ipcam
+    ];
   };
 }
