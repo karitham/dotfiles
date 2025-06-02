bf8101b88badbbb9c40b93b2f9dd15ad0df3a7a3
karitham <kar@karitham.dev>
2025-06-02 19:29:28 +0000

    reg: writefreely

--- hosts/reg/default.nix
+++ hosts/reg/default.nix
@@ -17,15 +17,36 @@
       enable = true;
       host = "0.0.0.0";
     };
+
+    writefreely = {
+      enable = true;
+      host = "notes.0xf.fr";
+      nginx.enable = true;
+      acme.enable = true;
+      settings = {
+        server.port = 3003;
+        app.host = "https://notes.0xf.fr";
+        app.site_name = "writefreely";
+        app.site_description = "writefreely";
+        app.single_user = true;
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [80 443];
+
+  security.acme = {
+    defaults.email = "netop@0xf.fr";
+    acceptTerms = true;
   };
 
   users.users = {
     ${username}.openssh.authorizedKeys.keyFiles = [inputs.ssh-keys];
   };
 
-  environment.systemPackages = with pkgs; [helix];
+  environment.systemPackages = with pkgs; [helix writefreely];
 
   system = {
-    stateVersion = "24.11";
+    stateVersion = "25.05";
   };
 }
