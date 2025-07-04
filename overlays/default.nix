2924e3c691d215046f282c3f92e1a2721b0a3099
karitham <kar@karitham.dev>
2025-07-04 19:30:44 +0000

    dead function arg names

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,13 +1,13 @@
 _: prev: {
   pokego = prev.callPackage ../pkgs/pokego.nix {};
-  golangci-lint-langserver = prev.golangci-lint-langserver.overrideAttrs (old: {
+  golangci-lint-langserver = prev.golangci-lint-langserver.overrideAttrs (_: {
     patches = prev.fetchurl {
       url = "https://github.com/karitham/golangci-lint-langserver/commit/31e6806187d431a8865261b5441ef5a65b589ae5.patch";
       hash = "sha256-Sw7KCsAhShobhvtwS0KeeWL4ewuXuQYyPeaeHKGUs+I=";
     };
     vendorHash = "sha256-kbGTORTTxfftdU8ffsfh53nT7wZldOnBZ/1WWzN89Uc=";
   });
-  gotools = prev.gotools.overrideAttrs (old: {
+  gotools = prev.gotools.overrideAttrs (_: {
     patches = prev.fetchurl {
       url = "https://github.com/karitham/gotools/commit/97818d312ebfc0e879de489035dee88e910fd95d.patch";
       hash = "sha256-2EYyelh/NmeO9PuCr5xlx9HhRrqfEjseXB7WLvdrJes=";
