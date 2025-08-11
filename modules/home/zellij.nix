033a39f83108b61e16a77c04ba3ddd7548bbaecf
karitham <kar@karitham.dev>
2025-08-11 16:15:50 +0200

    zellij config

--- modules/home/zellij.nix
+++ modules/home/zellij.nix
@@ -3,6 +3,12 @@ _: {
     enable = true;
     settings = {
       scroll_buffer_size = 5000;
+      default_shell = "nu";
+      post_command_discovery_hook = ''
+        direnv exec . -- ($env.RESURRECT_COMMAND)
+      '';
+      default_layout = "compact";
+      # default_mode = "locked";
     };
   };
 }
