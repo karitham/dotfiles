90de4ecac7a6ad3c79a67e135d20cd23a9e19dd6
karitham <kar@karitham.dev>
2025-08-26 18:11:59 +0200

    kiwi: zed

--- hosts/kiwi/desktop.nix
+++ hosts/kiwi/desktop.nix
@@ -1,9 +1,6 @@
 {lib, ...}: {
   programs.waybar.settings.mainBar.battery.bat = lib.mkForce "BAT0";
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "eDP-1, preferred, 0x0, 1"
-      "HDMI-A-1, preferred, auto-left, 1"
-    ];
+  programs.zed-editor = {
+    enable = true;
   };
 }
