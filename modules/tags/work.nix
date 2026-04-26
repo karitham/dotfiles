5c7fb3851b297486fc0e6bea18fed95985e87754
karitham <kar@karitham.dev>
2026-04-26 19:33:33 +0200

    work: fix top-level config

--- modules/tags/work.nix
+++ modules/tags/work.nix
@@ -3,30 +3,30 @@
   config = lib.mkIf config.desktop.enable {
     desktop.yubikey.enable = true;
     programs._1password.enable = true;
-  };
 
-  home-manager.users.${config.my.username} = {
-    xdg.configFile."git/config".text = ''
-      [includeIf "hasconfig:remote.*.url:git+ssh://git@github.com/upfluence/**"]
-          path = "~/upf/.gitconfig"
-      [includeIf "hasconfig:remote.*.url:git@github.com:upfluence/**"]
-          path = "~/upf/.gitconfig"
-    '';
+    home-manager.users.${config.my.username} = {
+      xdg.configFile."git/config".text = ''
+        [includeIf "hasconfig:remote.*.url:git+ssh://git@github.com/upfluence/**"]
+            path = "~/upf/.gitconfig"
+        [includeIf "hasconfig:remote.*.url:git@github.com:upfluence/**"]
+            path = "~/upf/.gitconfig"
+      '';
 
-    programs.opencode.settings.mcp = {
-      linear = {
-        type = "remote";
-        url = "https://mcp.linear.app/mcp";
-        enabled = false; # disable by default because it breaks google models
-        headers = {
-          Authorization = "Bearer {env:LINEAR_API_KEY}";
+      programs.opencode.settings.mcp = {
+        linear = {
+          type = "remote";
+          url = "https://mcp.linear.app/mcp";
+          enabled = false; # disable by default because it breaks google models
+          headers = {
+            Authorization = "Bearer {env:LINEAR_API_KEY}";
+          };
         };
-      };
-      sentry = {
-        type = "remote";
-        enabled = false;
-        url = "{env:SENTRY_MCP_URL}";
-        oauth = { };
+        sentry = {
+          type = "remote";
+          enabled = false;
+          url = "{env:SENTRY_MCP_URL}";
+          oauth = { };
+        };
       };
     };
   };
