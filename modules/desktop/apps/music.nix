2cb9ca9ad17b9e46a662d0af45b1c08cc51ca08d
karitham <kar@karitham.dev>
2026-09-11 11:25:36 +0200

    modules/desktop/apps/music.nix: drop ytm app

--- modules/desktop/apps/music.nix
+++ modules/desktop/apps/music.nix
@@ -1,14 +1,1 @@
-{
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-{
-  config = lib.mkIf config.desktop.enable {
-    home.packages = [
-      # youtube music in browser
-      (pkgs.ytmdesktop.override { commandLineArgs = "--password-store=gnome-libsecret"; })
-    ];
-  };
-}
+_: { }
